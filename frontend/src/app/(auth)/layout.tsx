9d38f808e0f3480af829abeba2dd9353938f0bc0
kovacsmate3 <kovmate3@gmail.com>
2025-04-05 00:49:09 +0200

    Changing the icon of the website (favicon editing).

---
+++ frontend/src/app/(auth)/layout.tsx
@@ -0,0 +1,60 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/stylesheets/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/(main)/Header";
+import Footer from "@/components/(main)/Footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "A-Ponton Kft.",
+  description: "CRUD Based Next js App with Laravel",
+  icons: {
+    icon: [
+      {
+        url: "/images/favicon.ico",
+        href: "/images/favicon.ico",
+      },
+    ],
+    apple: [
+      {
+        url: "/images/apple-touch-icon.png",
+        href: "/images/apple-touch-icon.png",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          enableSystem
+          attribute="class"
+          defaultTheme="system"
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ frontend/src/app/(main)/layout.tsx
@@ -0,0 +1,65 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/stylesheets/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/(main)/Header";
+import Footer from "@/components/(main)/Footer";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
+import ClientWrapper from "@/components/client-wrapper";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "A-Ponton Kft.",
+  description: "CRUD Based Next js App with Laravel",
+  icons: {
+    icon: [
+      {
+        url: "/images/favicon.ico",
+        href: "/images/favicon.ico",
+      },
+    ],
+    apple: [
+      {
+        url: "/images/apple-touch-icon.png",
+        href: "/images/apple-touch-icon.png",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          enableSystem
+          attribute="class"
+          defaultTheme="system"
+          disableTransitionOnChange
+        >
+          <ClientWrapper>
+            <Header />
+            {children}
+            <Footer />
+            <TailwindIndicator />
+          </ClientWrapper>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
