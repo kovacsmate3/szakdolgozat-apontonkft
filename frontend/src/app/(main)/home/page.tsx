c81a31911e25a7dc7a7f9bbeb0429b1985b38fc4
kovacsmate3 <kovmate3@gmail.com>
2025-04-07 17:08:17 +0200

    Updated imports in the Home page.

--- frontend/src/app/(main)/home/page.tsx
+++ frontend/src/app/(main)/home/page.tsx
@@ -1,6 +1,6 @@
-import AboutSection from "@/components/(main)/main/home/AboutSection";
-import HeroSection from "@/components/(main)/main/home/HeroSection";
-import PartnerSection from "@/components/(main)/main/home/partner/PartnerSection";
+import AboutSection from "@/components/(main)/home/about/AboutSection";
+import HeroSection from "@/components/(main)/home/HeroSection";
+import PartnerSection from "@/components/(main)/home/partner/PartnerSection";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
